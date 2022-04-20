77992b041e2c6b02d2d5daa41de15b45f182b926
Alex <alexander@borsh.pro>
2022-04-20 21:14:32 +0300

    ADD led check: 2M, LIMIT 5

--- water_level_t13.ino
+++ water_level_t13.ino
@@ -1,53 +1,58 @@
 #include <avr/io.h>
-#define SENSOR PB0
+#define SENSOR PB2
 #define LED PB3
+#define OUT_SIGNAL PB0
 
-void setup(){
+void setup() {
 	CLKPR &= ~((1<<0)|(1<<1)|(1<<2)|(1<<3)); // Clock Prescale Register to 0b0000 > divide by 1
 	// DDRB &= ~(1<<SENSOR);		// set SENSOR as input
 	// pinMode(SENSOR, INPUT);		// set SENSOR as input
 	// DDRB |= (1<<LED);			// set LED as output
 	pinMode(LED, OUTPUT);		// set LED as output
-	// PORTB |= (1<<LED);		// set LED HIGH
+	pinMode(OUT_SIGNAL, OUTPUT);		// set LED as output
 	digitalWrite(LED, HIGH);	// set LED HIGH (off)
+	// PORTB |= (1<<LED);		// set LED HIGH
 	// PORTB &= ~(1<<LED);		// set LED LOW
 
 	// Start
-	blink();
 
 	// Charge-discharge preparation
 	// Charge
 	pinMode(SENSOR, INPUT);
-	while (digitalRead(SENSOR) == 0) {
+	while (digitalRead(SENSOR) == LOW) {
 	}
 
 	// Discharge
 	pinMode(SENSOR, OUTPUT);
 	digitalWrite(SENSOR, LOW);
-	while (digitalRead(SENSOR)) {}
+	while (digitalRead(SENSOR) == HIGH) {}
 
 	// Check
 	// Charge
 	uint8_t count = 0;
 	pinMode(SENSOR, INPUT);
-	while (digitalRead(SENSOR) == 0) {
+	while (digitalRead(SENSOR) == LOW) {
 		count ++;
 	}
 
 	// Stop
-	blink();
-
-	// Pause
-	delay(250); // 2 seconds
 
 	// Result
+	if (count > 5) { // Full of Water
+		digitalWrite(OUT_SIGNAL, HIGH);	// HIGH output signal
+	} else { // Empty, no water
+		digitalWrite(OUT_SIGNAL, LOW);		// LOW output signal
+	}
+
 	for (uint8_t i=0; i<count; i+=1){
 		blink();
 	}
 
-	// On if water
-	if (count > 5) {
-		digitalWrite(LED, LOW);	// off
+	// LED
+	if (count > 5) { // Full of Water
+		digitalWrite(LED, HIGH);	// OFF
+	} else { // Empty, no water
+		digitalWrite(LED, LOW);		// ON
 	}
 }
 
@@ -56,8 +61,9 @@ void loop() {
 
 void blink() {
 	digitalWrite(LED, LOW);		// on
-	delay(50); // 400 ms
+	delay(20); // 160 ms
 	digitalWrite(LED, HIGH);	// off
-	delay(50); // 400 ms
+	delay(20); // 160 ms
+	// delay(50); // 400 ms
 	// delay(125); // 1 second
 }
